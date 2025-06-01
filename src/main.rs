ac7ab87f203aa5cb85da62f221937fd320f14183
Yufeireal <yufei.zheng@verkada.com>
2025-06-01 16:41:40 -0700

    finish networking code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use template::{network, Backend};
+use tokio::net::TcpListener;
+use tracing::{info, warn};
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let addr = "0.0.0.0:6379";
+    info!("Simple-Redis-Server is listening on {}", addr);
+    let listener = TcpListener::bind(addr).await?;
+    let backend = Backend::new();
+    loop {
+        let (stream, raddr) = listener.accept().await?;
+        info!("Accepted connections from {}", raddr);
+        let cloned_backend = backend.clone();
+        tokio::spawn(async move {
+            match network::stream_handler(stream, cloned_backend).await {
+                Ok(_) => {
+                    info!("Connection from {} exited", raddr);
+                }
+                Err(e) => {
+                    warn!("handle error from {}: {:?}", raddr, e);
+                }
+            }
+        });
+    }
 }
